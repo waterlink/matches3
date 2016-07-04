0ea97055ca1406e36aa279edc04c5dc9552f8059
Oleksii Fedorov <waterlink000@gmail.com>
2016-07-04 19:54:41 +0200

    Cell can be created with color

--- spec/PlayerSpec.js
+++ spec/PlayerSpec.js
@@ -1,5 +1,6 @@
 const Player = require("../src/Player")
 const GameField = Player.GameField
+const Cell = Player.Cell
 
 describe("Player", function () {
     it("can be created with game field", function () {
@@ -19,12 +20,8 @@ describe("GameField", function () {
     })
 })
 
-function Cell() {
-    
-}
-
 describe("Cell", function () {
-    it("can be created", function () {
-        var cell = new Cell()
+    it("can be created with color", function () {
+        var cell = new Cell(Cell.RED)
     })
 })
