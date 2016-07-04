63b3d38f819f35d06bf710ea132a0b9fdfc29a23
Oleksii Fedorov <waterlink000@gmail.com>
2016-07-04 19:48:06 +0200

    Added Player

---
+++ spec/PlayerSpec.js
@@ -0,0 +1,11 @@
+const Player = require("../src/Player")
+
+describe("Player", function () {
+    it("can be created", function () {
+        var player = new Player()
+    })
+})
+
+describe("GameField", function () {
+        
+})
---
+++ src/Player.js
@@ -0,0 +1,7 @@
+function Player() {
+
+}
+
+if (typeof module != "undefined") {
+    module.exports = Player
+}
